62ed8bdd6165ffd9d92df64e2bb32822e9b6c6eb
tinashemk <tinashekmakarudze@gmail.com>
2022-09-08 17:42:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 
 
 
-int main () {
+int main(void) {
 
-   puts("\"Programming is like building a multilingual puzzle");
+   puts ("\"Programming is like building a multilingual puzzle");
 
-   return(0);
+   return (0);
 
 }
 
