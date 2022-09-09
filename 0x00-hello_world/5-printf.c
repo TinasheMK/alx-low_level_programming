d10237eb14040b7274d835c487b119796737e879
tinashemk <tinashekmakarudze@gmail.com>
2022-09-08 18:09:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,10 @@
-
-
 #include <stdio.h>
-
-
-
-int main (){
-
-    printf(with proper grammar, but the outcome is a piece of art);
-
-    return(0);
-
+/**
+** main- Prints text
+** Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
 }
-
-
