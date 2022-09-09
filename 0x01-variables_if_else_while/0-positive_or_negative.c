a87aa4ab1ae23fa982607d549e5a8b5136baea97
tinashemk <tinashekmakarudze@gmail.com>
2022-09-09 12:54:13 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0)
     {
-        printf ("is positive")
+        printf("is positive")
     }
 if(n<0)
     {
-        printf ("is negative")
+        printf("is negative")
     }
 return (0);
 }
