d012372552a7db1c334467499ffc587b8cde1604
tinashemk <tinashekmakarudze@gmail.com>
2022-09-09 18:28:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+
+*main - put char
+
+*Description: prints alphabet
+
+*Return: 0
+
+*
+
+*/
+
+int main(void)
+
+{
+
+char ch;
+
+for(ch=a; ch<=z; ch++)
+
+{
+
+putchar("%c", ch);
+
+};
+
+putchar("\n", ch);
+
+return 0;
+
+}
+
+
