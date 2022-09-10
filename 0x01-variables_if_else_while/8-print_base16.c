d92bb18334575cd69fa4a06c8bd71df5f8591538
tinashemk <tinashekmakarudze@gmail.com>
2022-09-09 19:25:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - put char
+*Description: prints numbers
+*Return: 0
+*
+*/
+int main(void)
+{
+int n;
+for (n = 0x0 ; n <= 0xf; n++)
+{
+putchar(n);
+};
+printf('\n');
+return (0);
+}
