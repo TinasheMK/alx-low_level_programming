0fcd77d1755aedfa3ea5ed916d457bb246a684b7
tinashemk <tinashekmakarudze@gmail.com>
2022-09-15 16:41:26 -0700

    Keep going

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,18 @@
+#include <ctype.h>
 #include "main.h"
 /**
  ** _isupper - check for upper case letter
- *
  ** @c : character to check the case
- *
  ** Return:0 or 1
- *
  * */
 int _isupper(int c)
 {
-return (c >= 65 && c <= 90);
+  if (isupper(c))
+  {
+    return(0);
+  }
+  else
+  {
+    return(1);
+  }
 }
