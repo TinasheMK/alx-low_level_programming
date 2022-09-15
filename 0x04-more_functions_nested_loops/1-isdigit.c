fa079625e7ef1fe69005dc97e394478e6a0090a1
tinashemk <tinashekmakarudze@gmail.com>
2022-09-15 16:55:22 -0700

    Keep going

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ ** _isdigit - check for upper case letter
+ ** @c : character to check the case
+ ** Return:0 or 1
+ **/
+int _isupper(int c)
+{
+return (isdigit(c));
+}
