b1ad1a809bb54bf8781168a44ca48ce84d758cdb
Tinashe Makarudze <tinashekmakarudze@gmail.com>
2022-09-17 04:30:33 +0200

    c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,16 +12,39 @@ void positive_or_negative(int i);
 
 int main(void)
 {
-        int a, b, c;
-        int largest;
+    int a, b, c;
+    int largest;
+    a = 972;
+    b = -98;
+    c = 0;
+    largest = largest_number(a, b, c);
+    printf("%d is the largest number\n", largest);
+    return (0);
+}
 
-        a = 972;
-        b = -98;
-        c = 0;
+#include "main.h"
 
-        largest = largest_number(a, b, c);
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
 
-        printf("%d is the largest number\n", largest);
+int main(void)
+{
+    int month;
+    int day;
+    int year;
 
-        return (0);
+    month = 4;
+    day = 01;
+    year = 1997;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
+
+    print_remaining_days(month, day, year);
+
+    return (0);
 }
