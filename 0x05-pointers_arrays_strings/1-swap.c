986059c10a50e8e58aedaf9344fc45f41b5cab41
tinashemk <tinashekmakarudze@gmail.com>
2022-09-19 16:47:41 -0700

    Swap tow ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b;
+	*b = *a;
+	*a = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,12 @@
  * Return: 0
  */
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b);
