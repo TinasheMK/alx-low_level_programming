c331c43c26440a3740ce1c394ee5d7b371470ac7
tinashemk <tinashekmakarudze@gmail.com>
2022-09-19 16:31:34 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98: Points to int
+ * Return- 0
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+/**
+ * reset_to_98 - uses pointer
+ * @n: integer pointer
+ * Return: 0
+ */
+void reset_to_98(int *n);
