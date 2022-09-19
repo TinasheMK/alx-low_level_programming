fe091d6e55813adc054cec3a23a7e1f61e8f9381
tinashemk <tinashekmakarudze@gmail.com>
2022-09-19 16:58:24 -0700

    Len of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns string length
+ * @s: pointer to the string
+ *
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
