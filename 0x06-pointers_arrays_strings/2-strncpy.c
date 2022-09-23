4192a3aff7b9fd1a1c908d14f6baef42c3cfe1b6
Tinashe Makarudze <tinashekmakarudze@gmail.com>
2022-09-23 04:36:39 +0200

    h

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,18 @@
-#include "main.h"
-#include <string.h>
 /**
- * *_strncpy - cpy a string
- * @dest: var
- * @src: pointer var
- *
- * Return: void
+ * _strncpy - copies a string up to n
+ * @dest: Destination of the pointer to the string
+ * @src: Source of the pointer to the string to copy
+ * @n: Number of bytes to copy.
+ * Return: Pointer to the destination string.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i]	= src[i];
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
