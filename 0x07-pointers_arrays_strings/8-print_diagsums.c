cb6706c9644caab857e58568725e33bcd629165c
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2022-09-27 04:32:03 +0200

    last

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_diagsums -  prints sums
- * @a: a pointer
+ * print_diagsums -  sums
+ * @a: a point
  * @size: size
  */
 void print_diagsums(int *a, int size)
