a56c026005eb2100273d586647ac09f0ea8db856
tinashemk <tinashekmakarudze@gmail.com>
2022-09-26 18:49:36 -0700

    Q6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - main
+ * @a: pointer
+ *
+ * Return: chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 8)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+		i++;
+	}
+}
